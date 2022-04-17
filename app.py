e66dea462bec80e94711ac79c0bd3b3923fac792
Muzamilkhan-Waveit <muzamilkhan111814@gmail.com>
2022-04-18 00:21:39 +0500

    V2.0

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from model import check_file
+from model import predict_fields
 import os
 a = Flask(__name__)
 
@@ -13,14 +13,44 @@ def home():
 
 @a.route("/result", methods = ['POST'])
 def result():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-           file_path = os.path.join(a.config['UPLOAD_FOLDER'], uploaded_file.filename)
-          # set the file path
-           uploaded_file.save(file_path)
-           predection = check_file(file_path)
-           return render_template("index.html", pred=predection)
+    if request.method == "POST":
+        value1 = request.form["value1"]
+        value2 = request.form["value2"]
+        value3 = request.form["value3"]
+        value4 = request.form["value4"]
+        value5 = request.form["value5"]
+        value6 = request.form["value6"]
+        value7 = request.form["value7"]
+        value8 = request.form["value8"]
+        value9 = request.form["value9"]
+        value10 = request.form["value10"]
+        value11 = request.form["value11"]
+        value12 = request.form["value12"]
+        value13 = request.form["value13"]
+        value14 = request.form["value14"]
+        value15 = request.form["value15"]
+        all_values = [value1,
+                value2, 
+                value3,
+                value4,
+                value5,
+                value6,
+                value7,
+                value8,
+                value9,
+                value10,
+                value11,
+                value12,
+                value13,
+                value14,
+                value15]
+        new = [int(x) for x in all_values]
+
+        predection = predict_fields(new)
+        return render_template("index.html", pred=predection)
     else:
         return render_template("index.html", pred = "a")
+
+
 if __name__ == '__main__':
-    a.run(debug=False)
+    a.run(debug=True)
--- model.py
+++ model.py
@@ -1,14 +1,14 @@
-
 import pandas as pd
-
-import pickle
+import pickle 
+import numpy as np
 with open('model_pkl' , 'rb') as f:
     model = pickle.load(f)
 
-def check_file(file):
-    data = pd.read_csv(file)
-    if data.shape != (1,144):
-        return "Invalid File"
-    result = model.predict(data)
-    print(result[0])
-    return result[0]
+
+
+def predict_fields(myArray):
+    values = np.array(myArray)
+    new = np.expand_dims(values,axis=0)
+    myVal =  model.predict(new)
+    print(myVal)
+    return myVal
