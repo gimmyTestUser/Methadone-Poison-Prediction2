817344d2e9814316a0ce64516a1edf6574642b1a
Muzamilkhan-Waveit <muzamilkhan111814@gmail.com>
2022-04-20 08:49:09 +0500

    my Commit

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import pickle 
 import numpy as np
-with open('model_pkl' , 'rb') as f:
-    model = pickle.load(f)
+# with open('model_pkl' , 'rb') as f:
+#     model = pickle.load(f)
 
 
 
 def predict_fields(myArray):
     values = np.array(myArray)
     new = np.expand_dims(values,axis=0)
-    myVal =  model.predict(new)
-    print(myVal)
-    return myVal
+    # myVal =  model.predict(new)
+    # print(myVal)
+    return "myVal"
